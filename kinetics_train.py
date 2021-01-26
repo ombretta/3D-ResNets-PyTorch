28835cf701b8b31f8864a8d683147aeb7f7ad591
Ombretta Strafforello <ombrettastraff@login1.hpc.tudelft.nl>
2021-01-26 13:00:49 +0100

    runs

--- kinetics_train.py
+++ kinetics_train.py
@@ -128,11 +128,11 @@ n_threads = 4
 # Trained model is saved at every this epochs.
 checkpoint = 5
 # (resnet | resnet2p1d | preresnet | wideresnet | resnext | densenet | vidbagnet |
-model = 'resnet'
+model = 'vidbagnet'
 # Depth of resnet (10 | 18 | 34 | 50 | 101)
 model_depth = 50
 # Depth of resnet (9 | 17 | 33)
-receptive_size = 9
+receptive_size = 33
 # Kernel size in t dim of conv1.
 conv1_t_size = 7
 # Stride in t dim of conv1.
