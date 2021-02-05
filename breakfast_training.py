cc7bd7bfce8c341c42804ac445e1d43cbca5acbb
Ombretta Strafforello <ombrettastraff@login1.hpc.tudelft.nl>
2021-02-05 11:43:19 +0100

    runs

--- breakfast_training.py
+++ breakfast_training.py
@@ -48,9 +48,9 @@ pretrain_path = "3D-ResNets-PyTorch/pretrained_models/r3d50_K_200ep.pth"
 # Module name of beginning of fine-tuning (conv1, layer1, fc, denseblock1, classifier, ...). The default means all layers are fine-tuned.
 ft_begin_module = ''
 # Height and width of inputs
-sample_size = 224 #Default: 64
+sample_size = 128 #Default: 64
 # Temporal duration of inputs
-sample_duration = 8
+sample_duration = 512
 # If larger than 1, input frames are subsampled with the stride.
 sample_t_stride = 1 #default: 15, 15fps
 # Spatial cropping method in training. random is uniform. corner is selection from 4 corners and 1 center. random | corner | center)
@@ -94,7 +94,7 @@ overwrite_milestones = False
 # Patience of LR scheduler. See documentation of ReduceLROnPlateau.
 plateau_patience = 10
 # Batch Size
-batch_size = 8
+batch_size = 4
 # Batch Size for inference. 0 means this is the same as batch_size.
 inference_batch_size = 0
 # If true, SyncBatchNorm is used instead of BatchNorm.
