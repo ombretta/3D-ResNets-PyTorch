3381fb6302a0d559adcbade718caa0d1a45dae6d
Ombretta <o.strafforello@tudelft.nl>
2021-06-04 12:28:30 +0200

    fixed test files

--- analyze_tb_files.py
+++ analyze_tb_files.py
@@ -14,33 +14,40 @@ import json
 
 if not os.path.exists("plots/"): os.mkdir("plots/")
 
-plots_name = "movingmnist_longterm_1_2_4"
+plots_name = "movingmnist_longterm_resnet50_vs_videobagnet_tem9_10videosxdigit"
 
 res_dirs = [f for f in os.listdir("results/") if "mnist" in f]
-discard_dirs = ["motion", "blackframes", "val_1tstride"]
+discard_dirs = ["longterm", "motion", "blackframes", "val_1tstride"]
 res_dirs = [f for f in res_dirs if all([d not in f for d in discard_dirs])]
 
 # Filter according to training setting
 colors = ["red", "green", "orange", "black"]
-filtering_criteria1 = ["resnet_18"]
+filtering_criteria1 = ["resnet_50", "bagnet_tem_9"] #["32frames", "64frames", "128frames"]
 # filtering_criteria1 = [""] #["bagnet_tem", "resnet_18", "resnet_50"]
-filtering_criteria2 = [""]
-filtering_criteria3 = [""] #["256"]
-filtering_criteria_annotation_path = ["longterm/"]
-filtering_criteria_sampling = ["even_crops"]
-
+filtering_criteria2 = ['32frames_32'] #["64frames"]
+filtering_criteria3 = [""] #["32frames_8", "64frames_4", "128frames_2", "256"] #["256"]
+filtering_criteria_annotation_path = [''] #['mnist_json_10.json'] #["longterm/"]
+filtering_criteria_sampling = [''] #["even_crops"]
 fig_train, ax_train = plt.subplots(figsize=(12, 8))
 fig_val, ax_val = plt.subplots(figsize=(12, 8))
 
 for r in res_dirs:
     
+    if os.path.exists("results/"+r+"/opts.json"):
+            with open("results/"+r+"/opts.json", "r") as f:
+                opts = json.load(f)
+            # print(opts["annotation_path"])
+            if "mnist_json_10.json" in opts["annotation_path"]: 
+                print(r)
+    
     low_data_regime = False
-
-    # print(r)
+    
     if [f for f in os.listdir("results/"+r) if "events.out" in f] and \
         any([c in r for c in filtering_criteria1]) and \
         any([c in r for c in filtering_criteria2]) and \
         any([c in r for c in filtering_criteria3]):
+            
+        # print(r)
         event_acc = EventAccumulator("results/"+r)
         event_acc.Reload()
         # Show all tags in the log file
@@ -50,13 +57,14 @@ for r in res_dirs:
         if os.path.exists("results/"+r+"/opts.json"):
             with open("results/"+r+"/opts.json", "r") as f:
                 opts = json.load(f)
-            if "mnist_json_100.json" in opts["annotation_path"]: 
-                low_data_regime = True
+            print(opts["annotation_path"])
+            # if "mnist_json_10.json" in opts["annotation_path"]: 
+            #     print(r)
+            #     low_data_regime = True
             # print(opts)
-            print(opts.keys())
-            # print(opts['annotation_path'])
         
-        if (False or low_data_regime) and \
+
+        if (True or low_data_regime) and \
             any([c in opts['annotation_path'] for c in filtering_criteria_annotation_path]) \
             and any ([c in opts['train_t_crop'] for c in filtering_criteria_sampling]):
             if event_acc.scalars.Keys() != []:
@@ -64,30 +72,37 @@ for r in res_dirs:
                 val_losses, val_epochs, val_accs = zip(*event_acc.Scalars('val/acc'))
                 if len(val_losses) <10:
                     os.system("rm -r results/"+r)
-                if len(val_losses) >= 20 or train_accs[-1] > 0.95:
-                    print(r)
-                    if low_data_regime: print("low_data_regime")
+                if len(val_losses) >= 10: # or train_accs[-1] > 0.95:
+                    
                     # print(len(val_losses))
+                    # print(r)
                     print("train", round(np.max(train_accs)*100, 2), np.argmax(train_accs), \
                           "val", round(np.max(val_accs)*100, 2), np.argmax(val_accs))
                     
+                    if os.path.exists("results/"+r+"/checkpoints_test_results.json"):
+                        with open("results/"+r+"/checkpoints_test_results.json", "r") as f:
+                            test = json.load(f)
+                        print(test)
+                        
                     color = [colors[i] for i in range(len(filtering_criteria1)) \
                               if [c in r for c in filtering_criteria1][i]][0]
                     
-                    color = [colors[i] for i in range(len(filtering_criteria_annotation_path)) \
-                              if [c in opts['annotation_path'] for c in filtering_criteria_annotation_path][i]][0]
+                    # color = [colors[i] for i in range(len(filtering_criteria_annotation_path)) \
+                    #           if [c in opts['annotation_path'] for c in filtering_criteria_annotation_path][i]][0]
                         
                     frequency = [c in opts['annotation_path'] for c in filtering_criteria1][0]
                     linestyle='dashed' if low_data_regime else 'solid'
                     if np.max(val_accs)*100 > 10:
-                        ax_train.plot(train_epochs, train_accs, label=r+str(frequency), color=color, 
+                        ax_train.plot(train_epochs, [t*100 for t in train_accs], 
+                                      label=r+str(frequency), color=color, 
                                       linewidth=2, linestyle=linestyle)
                         ax_train.set_title("Training accuracies")
                         ax_train.set_xlabel("Epochs")        
                         ax_train.set_ylabel("Train acc (%)")        
                         ax_train.legend()
                         
-                        ax_val.plot(val_epochs, val_accs, label=r, color=color, 
+                        ax_val.plot(val_epochs, [v*100 for v in val_accs],
+                                    label=r, color=color, 
                                     linewidth=2, linestyle=linestyle)
                         ax_val.set_title("Validation accuracies")
                         ax_val.set_xlabel("Epochs")        
