170be5af1fc309526ec40c837e4334d17566e8be
Ombretta <o.strafforello@tudelft.nl>
2021-06-14 09:45:20 +0200

    runs

--- analyze_tb_files.py
+++ analyze_tb_files.py
@@ -23,9 +23,9 @@ res_dirs = [f for f in res_dirs if all([d not in f for d in discard_dirs])]
 # Filter according to training setting
 colors = ["red", "green", "orange", "black"]
 filtering_criteria1 = ["resnet_50", "resnet_18", "bagnet_tem_17", "bagnet_tem_9"] #["32frames", "64frames", "128frames"]
-filtering_criteria2 = ["frequencies"] #["64frames"]
-filtering_criteria3 = [""] #["32frames_8", "64frames_4", "128frames_2", "256"] #["256"]
-filtering_criteria_annotation_path = [''] #["longterm/"]
+filtering_criteria2 = ["longterm"] #["64frames"]
+filtering_criteria3 = ["256"] #["32frames_8", "64frames_4", "128frames_2", "256"] #["256"]
+filtering_criteria_annotation_path = ['100.json', "json.json"] #["longterm/"]
 filtering_criteria_sampling = [""]
 fig_train, ax_train = plt.subplots(figsize=(12, 8))
 fig_val, ax_val = plt.subplots(figsize=(12, 8))
